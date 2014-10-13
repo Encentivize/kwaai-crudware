7c13a8563d1ac9c187b81a1fdea702d1f9c1d6e8
unknown <martin@MartinN-PC.entelect.local>
2014-10-13 17:28:59 +0200

    Updated to new stack

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-﻿module.exports = require('./lib/crudware.js');
+﻿module.exports.utils = require('./lib/utils.js');
+module.exports.crudWare=require("./lib/crudware.js")
 
 
 
--- testing.js
+++ testing.js
@@ -1,1 +1,62 @@
-var kwaaiMongo=require('./lib/crudware.js');
+var kwaaiCrudwareUtils=require('./index.js').utils;
+var express=require("express");
+var connectionString="mongodb://127.0.0.1:27017/testdb";
+var http=require("http");
+var bodyParser=require('body-parser');
+
+var app=express();
+var kwaaiCrudware=require('./index.js').crudWare(connectionString);
+app.set('port', process.env.PORT || 1337);
+app.use(bodyParser.json());
+
+var schema={
+    properties:{
+        name:{type:"string"},
+        description:{type:"string"}
+    },
+    required:["name"]
+}
+
+
+
+app.get("/test",
+    function(req,res,next){
+        req.user={roles:["test1"]};
+        next();
+    },
+    kwaaiCrudwareUtils.onlyForRoles(["test"]),
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",sendresponse:true}),
+    kwaaiCrudware.getByQuery
+);
+
+app.post("/test/validate",
+    function(req,res,next){
+        next();
+    },
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",schema:schema,sendresponse:true,validate:true}),
+    kwaaiCrudwareUtils.validateData
+);
+
+
+app.get("/test/:id",
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",sendresponse:true}),
+    kwaaiCrudware.getById
+);
+
+app.post("/test",
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",schema:schema,sendresponse:true}),
+    kwaaiCrudware.insert
+);
+app.put("/test/:id",
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",schema:schema,sendresponse:true}),
+    kwaaiCrudware.updateFull
+);
+
+app.patch("/test/:id",
+    kwaaiCrudwareUtils.setKwaaiOptions({collection:"test collection",schema:schema,sendresponse:true}),
+    kwaaiCrudware.updatePart
+);
+
+
+var server=http.createServer(app);
+server.listen(app.get('port'), function(){console.log('Express server listening on port ' + app.get('port'));});
